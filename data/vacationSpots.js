9438e8fc1f2f88987281ec7c8d518e4866d5cebb
Justin Lee <jlee2404@yahoo.com>
2025-04-17 18:57:58 -0400

    updated createSpot function

--- data/vacationSpots.js
+++ data/vacationSpots.js
@@ -1,31 +1,46 @@
 import {vacationSpots} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb';
 
-const exportedMethods = { // hello
-    async createPost(
+const exportedMethods = {
+// admins are able to create vacation spots
+async createSpot(
+    location,
+    name,
+    description
+){  
+
+    if (!location || !name || !description) {
+        throw ('ERROR: Missing required fields.');
+    }
+    const stringFields = { location, name, description };
+    for (const [key, value] of Object.entries(stringFields)) {
+        if (typeof value !== 'string' || value.trim().length === 0) {
+            throw ('ERROR: string inputs must be a non-empty string');
+        }
+    }
+    location = location.trim();
+    name = name.trim();
+    description = description.trim();
+
+    let reviews = [];
+    let foodRating = 0;
+    let safetyRating = 0;
+    let activityRating = 0;
+    let overallRating = 0;
+
+    const newPost = {
         location,
         name,
-        rank,
+        // rank,            // created spot will start with lowest rank
         description,
-        traditions,
         foodRating,
         safetyRating,
         activityRating,
-        overallRating
-    ){
-        const newPost = {
-            location: location,
-            name: name,
-            rank: rank,
-            description: description,
-            traditions: traditions,
-            foodRating: foodRating,
-            safetyRating: safetyRating,
-            activityRating: activityRating,
-            overallRating: overallRating
-        }
-
-        return newPost;
+        overallRating,
+        reviews
     }
+
+    return newPost;
+}
 }
 export default exportedMethods;
