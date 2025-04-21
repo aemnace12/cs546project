12211317aa87bd446adec9fed7d61ee56198df9b
Justin Lee <jlee2404@yahoo.com>
2025-04-21 10:43:01 -0400

    added users.js

---
+++ data/users.js
@@ -0,0 +1,14 @@
+import {vacationSpots} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+
+const exportedMethods = {
+async createUser () {
+
+},
+
+async removeUser () {
+
+}
+};
+
+export default exportedMethods;
