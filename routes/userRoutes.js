1e547081038b9d6e0f7a9f2569eefc0e5d22612d
jscheckn <schecknerjm@yahoo.com>
2025-04-25 20:46:49 -0400

    Added page and route for error, login, logout, and register, also added validation js file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 const router = Router();
 import {userData} from '../data/index.js';
+import validation from '../validation.js'
 
 
 
@@ -12,17 +13,80 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   res.json({route: '/users', method: req.method});
 });
-router.post('/login', async (req, res) => {
-  /*get req.body username and password
-    const { username, password } = req.body;
-    here, you would get the user from the db based on the username, then you would read the hashed pw
-    and then compare it to the pw in the req.body
-    let match = bcrypt.compare(password, 'HASHED_PW_FROM DB');
-    if they match then set req.session.user and then redirect them to the private page
-     I will just do that here */
-  req.session.user = {firstName: 'Patrick', lastName: 'Hill', userId: 123};
-  res.redirect('/private');
-});
+router
+  .route('/login')
+  .get(async (req, res) => {
+    //code here for GET
+    try{
+      res.render('auth/login')
+    }catch(e){
+      res.sendStatus(500);
+    }
+  })
+  .post(async (req, res) => {
+    //code here for POST
+    const regBody = req.body;
+    
+    try{
+      
+      regBody.userId=validation.checkString(regBody.userId);
+      regBody.userId=regBody.userId.toLowerCase();
+
+      regBody.password=validation.checkString(regBody.password);
+
+      //userId Logic
+      if(!(/^[A-Za-z0-9]+$/).test(regBody.userId)){
+        throw "userID contains non-letters/numbers"
+      }
+      if(regBody.userId.length > 10 || regBody.userId.length < 5){
+        throw "userId is wrong length"
+      }
+      regBody.userId=regBody.userId.toLowerCase();
+
+      //password logic
+      if(regBody.password.length < 8){
+        throw "password too short"
+      }
+      let chars = Array.from(regBody.password);
+      let uppercase = chars.some(char => /[A-Z]/.test(char));
+      let specialChar = chars.some(char => /[^a-zA-Z0-9 ]/.test(char));
+      let number = chars.some(char => !isNaN(char) && char !== ' ');
+      if(!specialChar){
+        throw "needs special char"
+      }
+      if(!uppercase){
+        throw "needs uppercase char"
+      }
+      if(!number){
+        throw "needs number"
+      }
+      //Login function needed from userData page
+      const reg = await login(regBody.userId, regBody.password);
+      
+      if(!reg){
+        return res.status(400).render('user/login', {error: "Invalid username or password"});
+      }
+      
+      req.session.user = {
+        firstName: reg.firstName,
+        lastName: reg.lastName,
+        userId: reg.userId,
+        role: reg.role,
+      };
+      
+      if(reg.role === "admin"){
+        
+        res.redirect("/admin") //NOT DONE
+      }else if(reg.role === "user"){
+        res.redirect("/profile")
+      }else{
+        throw "incorrect role"
+      }
+
+    }catch(e){
+      return res.status(400).render('error', {error: e});
+    }
+  });
 
 // sign up/register page 
 
@@ -30,13 +94,96 @@ router.get('/profile', async (req, res) => {
   res.render('user/profile');
 });
 
-router.get('/signup', async (req, res) => {
-  res.render('signup', {title: 'Sign Up'});
-});
+router
+  .route('/register')
+  .get(async (req, res) => {
+    //code here for GET
+    try{
+      res.render('auth/register')
+    }catch(e){
+      return res.sendStatus(500).render("error", {error: e});
+      
+    }
+  })
+  .post(async (req, res) => {
+    //code here for POST
+    const regBody = req.body;
+    try {
+      //Validate user input for registering
+      regBody.firstName = validation.checkString(regBody.firstName);
+      regBody.lastName = validation.checkString(regBody.lastName);
+      regBody.userId = validation.checkString(regBody.userId);
+      regBody.password = validation.checkString(regBody.password);
+      regBody.confirmPassword = validation.checkString(regBody.confirmPassword);
+      regBody.role = validation.checkString(regBody.role);
 
-router.post('/signup', async (req, res) => {
-    // prob a function to create the user in the database
-});
+      if(!(/^[A-Za-z]+$/).test(regBody.firstName)){
+        throw "first name contains non-letters"
+      }
+      if(regBody.firstName.length > 20 || regBody.firstName.length < 2){
+        throw "first name is wrong length"
+      }
+      
+      
+      if(!(/^[A-Za-z]+$/).test(regBody.lastName)){
+        throw "last name contains non-letters"
+      }
+      if(regBody.lastName.length > 20 || regBody.lastName.length < 2){
+        throw "last name is wrong length"
+      }
+
+      if(!(/^[A-Za-z0-9]+$/).test(regBody.userId)){
+        throw "userID contains non-letters/numbers"
+      }
+      if(regBody.userId.length > 10 || regBody.userId.length < 5){
+        throw "userID is wrong length"
+      }
+      regBody.userId=regBody.userId.toLowerCase();
+    
+      regBody.password=validation.checkString(regBody.password);
+      if(regBody.password.length < 8){
+        throw "password too short"
+      }
+      if(regBody.confirmPassword !== regBody.password){
+        throw "passwords are not the same"
+      }
+      let chars = Array.from(regBody.password);
+      let uppercase = chars.some(char => /[A-Z]/.test(char));
+      let specialChar = chars.some(char => /[^a-zA-Z0-9 ]/.test(char));
+      let number = chars.some(char => !isNaN(char) && char !== ' ');
+      if(!specialChar){
+        throw "needs special char"
+      }
+      if(!uppercase){
+        throw "needs uppercase char"
+      }
+      if(!number){
+        throw "needs number"
+      }
+
+    
+      regBody.role=validation.checkString(regBody.role);
+      regBody.role=regBody.role.toLowerCase();
+      if(regBody.role !== "admin" && regBody.role !== "user"){
+        throw "incorrect role";
+      }
+
+      //This function is needed! from userData
+      const reg = await register(regBody.firstName, regBody.lastName, regBody.userId, regBody.password, regBody.favoriteQuote, themePreference, regBody.role);
+
+      if(reg.registrationCompleted){
+        res.redirect("login")
+        
+      }else{
+        
+        res.status(500).json({error: "Internal Server Error"})
+      }
+    } catch (e) {
+      return res.status(400).render('auth/register', {error: e});
+    }
+
+  });
+
 router.get('/logout', async (req, res) => {
   req.session.destroy();
   res.send('Logged out');
