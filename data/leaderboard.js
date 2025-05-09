5ee5e1159d161be95e7b5c6cf0b5efba4415e4fc
Justin Lee <jlee2404@yahoo.com>
2025-05-09 13:52:50 -0400

    added some filtering options

--- data/leaderboard.js
+++ data/leaderboard.js
@@ -2,8 +2,30 @@ import {vacationSpots} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb';
 
 const exportedMethods = {
-    async fakeFunction(){
-        return null;
-    } 
+async sortByOverallRating(){
+    const locationCol = await vacationSpots();
+    sortedLocations = locationCol.sort((a, b) => b.overallRating - a.overallRating);
+    return sortedLocations;
+},
+async sortByFoodRating(){
+    const locationCol = await vacationSpots();
+    sortedLocations = locationCol.sort((a, b) => b.foodRating - a.foodRating);
+    return sortedLocations;
+},
+async sortBySafetyRating(){
+    const locationCol = await vacationSpots();
+    sortedLocations = locationCol.sort((a, b) => b.safetyRating - a.safetyRating);
+    return sortedLocations;
+},
+async sortByActivityRating(){
+    const locationCol = await vacationSpots();
+    sortedLocations = locationCol.sort((a, b) => b.activityRating - a.activityRating);
+    return sortedLocations;
+}, 
+async sortByAlphabet(){
+    const locationCol = await vacationSpots();
+    sortedLocations = locationCol.sort((a, b) => a.name.localeCompare(b.name));
+    return sortedLocations;
 }
+};
 export default exportedMethods;
