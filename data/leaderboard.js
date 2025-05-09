281c00f465f0e3a9ad5851cfe5199f3c5d064591
Justin Lee <jlee2404@yahoo.com>
2025-05-09 13:54:54 -0400

    fixed tiny mistake in filtering options

--- data/leaderboard.js
+++ data/leaderboard.js
@@ -4,27 +4,27 @@ import {ObjectId} from 'mongodb';
 const exportedMethods = {
 async sortByOverallRating(){
     const locationCol = await vacationSpots();
-    sortedLocations = locationCol.sort((a, b) => b.overallRating - a.overallRating);
+    const sortedLocations = locationCol.sort((a, b) => b.overallRating - a.overallRating);
     return sortedLocations;
 },
 async sortByFoodRating(){
     const locationCol = await vacationSpots();
-    sortedLocations = locationCol.sort((a, b) => b.foodRating - a.foodRating);
+    const sortedLocations = locationCol.sort((a, b) => b.foodRating - a.foodRating);
     return sortedLocations;
 },
 async sortBySafetyRating(){
     const locationCol = await vacationSpots();
-    sortedLocations = locationCol.sort((a, b) => b.safetyRating - a.safetyRating);
+    const sortedLocations = locationCol.sort((a, b) => b.safetyRating - a.safetyRating);
     return sortedLocations;
 },
 async sortByActivityRating(){
     const locationCol = await vacationSpots();
-    sortedLocations = locationCol.sort((a, b) => b.activityRating - a.activityRating);
+    const sortedLocations = locationCol.sort((a, b) => b.activityRating - a.activityRating);
     return sortedLocations;
 }, 
 async sortByAlphabet(){
     const locationCol = await vacationSpots();
-    sortedLocations = locationCol.sort((a, b) => a.name.localeCompare(b.name));
+    const sortedLocations = locationCol.sort((a, b) => a.name.localeCompare(b.name));
     return sortedLocations;
 }
 };
