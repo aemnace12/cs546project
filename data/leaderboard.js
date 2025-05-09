1c738d66047812e8334a392736087996d2acb705
gsokolik1 <gsokolik04@gmail.com>
2025-05-09 18:01:06 -0400

    beginning ajax

--- data/leaderboard.js
+++ data/leaderboard.js
@@ -2,8 +2,11 @@ import {vacationSpots} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb';
 
 const exportedMethods = {
-    async fakeFunction(){
-        return null;
-    } 
+   async sortby(value){
+    //https://www.geeksforgeeks.org/mongodb-sort-method/
+    const locationCol = await vacationSpots();
+    const locations = await locationCol.find().sort({[value]: -1});
+    return locations;
+    }
 }
 export default exportedMethods;
