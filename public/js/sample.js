6e6178dea0f68a0736baed7faf5abca1b49c93d5
gsokolik1 <gsokolik04@gmail.com>
2025-05-09 01:32:35 -0400

    beginning ajax

--- public/js/sample.js
+++ public/js/sample.js
@@ -1,1 +1,17 @@
 //used to take up space for further use
+
+(function ($) {
+    $('#sortByFood').on('click', function (event) {
+        event.preventDefault();
+        $('#leaderboard').empty();
+
+        let infoRequest = {
+          method: 'GET',
+          url: `/leaderboard/sortByFood`,
+        };
+    
+        $.ajax(infoRequest).then(function () {
+        
+        });
+      });
+    })(window.jQuery);
